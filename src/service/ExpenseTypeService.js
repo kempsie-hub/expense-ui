7e3a1947595c56f55755db1d33a5d5319f8e4f5c
nkempegowda <nkempegowda@gmail.com>
2025-03-02 14:45:02 +0530

    EXPENSE-UI: service port change

--- src/service/ExpenseTypeService.js
+++ src/service/ExpenseTypeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const EXPENSE_TYPES_BASE_URL = "http://localhost:8080/api/expense-types";
+const EXPENSE_TYPES_BASE_URL = "http://localhost:8081/api/expense-types";
 
 const EMPLOYEE_ID = 2;
 
